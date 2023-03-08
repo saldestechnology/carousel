5b7060dede1e8635670b77ddb66a3e09514e424d
Johan Saldes <johan@saldestechnology.com>
2023-03-08 23:35:21 +0100

    updated production url

--- utils/api.ts
+++ utils/api.ts
@@ -1,4 +1,2 @@
 export const baseUrl =
-  process.env.NODE_ENV === "production"
-    ? (process.env.API_URL as string)
-    : "http://localhost:3000";
+  process.env.NODE_ENV === "production" ? "" : "http://localhost:3000";
